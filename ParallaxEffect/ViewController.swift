1a2c1c7ddff4a45b03a4a36f80b4b2b517c57b1b
Katya Nerush <kate.nerush@conkergroup.com>
2015-09-28 17:21:23 +0100

    initial commit

--- ParallaxEffect/ViewController.swift
+++ ParallaxEffect/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var topImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = UIColor.clearColor()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK: - table delegate
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 200.0
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRectMake(0, 0, self.view.frame.size.width, 200));
+        view.backgroundColor = UIColor.clearColor()
+        
+        return view
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let scrollOffset = -scrollView.contentOffset.y * 0.5
+        let yPos = scrollOffset;
+        
+        topImage.frame = CGRectMake(0, yPos, topImage.frame.size.width, topImage.frame.size.height);
+    }
+    
 
 }
 
